86540e4bb678ee92f2652bfbe8da3dc22317fe5f
Travier Moorlag <moorlagt@gmail.com>
2024-02-15 18:53:21 -0500

    testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
-import { Context } from "hono";
+import { Context, Env, Input } from "hono";
 
 export function canHandleContext(c: Context) {
+  console.log(c.req.header("content-type"));
+}
+
+export function canHandleContextTwo<
+  T extends Env,
+  P extends string,
+  V extends Input
+>(c: Context<T, P, V>) {
   console.log(c.req.header("content-type"));
 }
